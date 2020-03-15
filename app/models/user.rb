9dcd6ccdd2f1729e97a146f0b9170febb4f145b5
Jonathan Kamens <jik@kamens.us>
2020-03-15 12:50:21 -0400

    Add role enum to user model
    Added a role enum to the user model, with two valid roles, "user" and
    "admin", following the instructions at
    https://github.com/heartcombo/devise/wiki/How-To:-Add-an-Admin-Role .

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  after_initialize :set_default_role, :if => :new_record?
+
+  enum role: [:user, :admin]
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
