780b70a3d40eae14105b7bb86a13d5a600d3f532
Jonathan Kamens <jik@kamens.us>
2020-03-15 13:45:06 -0400

    User Sendgrid to send email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,15 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: ENV["HOST_NAME"] }
 
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
